97cefd8dd4e401ceb056cfe55615566cc844de84
Weijun Huang <69240466+shijua@users.noreply.github.com>
2025-04-07 18:20:11 +0100

    feat: adding some basic stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,64 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://weijun-huang.netlify.app/`,
+    siteUrl: `https://shijua.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Weijun Huang',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Weijun's Personal Site`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `3rd Year Master of Computing Student at Imperial College London`,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@weijunhuang5`,
     // Optional: Github account URL
-    github: `https://github.com/RyanFitzgerald`,
+    github: `https://github.com/shijua`,
     // Optional: LinkedIn account URL
-    linkedin: `https://github.com/RyanFitzgerald/devfolio`,
+    linkedin: `https://www.linkedin.com/in/weijun-huang-793b3224b/`,
     // Content of the About Me section
     about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: '111Devfolio',
+        name: 'WACC compiler',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          '•	Led a team as Leader, effectively managing tasks and ensuring deadlines were met\n•	Developed and implemented the parser, semantic checker, and code generator in Rust\n•	Tested and validated the compiler on complex codebases like hash table and Tic-Tac-Toe, identifying edge cases and optimizing performance\n•	Enhanced and optimized the compiler to support advanced features like dynamic type inference, constant folding, and control flow analysis',
+        link: 'https://github.com/shijua/WACC-project',
       },
       {
-        name: 'ExtensionKit',
+        name: 'Pintos',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://extensionkit.io/?ref=devfolio',
+          '• Implemented core OS features such as priority scheduling, user program support, and virtual memory management, enhancing overall system functionality\n• Conducted extensive debugging and testing to ensure accuracy, stability, and reliability\n• Deepened understanding of OS principles through hands-on experience, applying concepts like process management and memory handling in real-world scenarios',
+        link: 'https://github.com/shijua/pintos',
       },
       {
-        name: 'Another Cool Project',
+        name: 'C Project',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          '• Coordinated team efforts in planning and execution, ensuring collaboration and project delivery\n• Successfully developed an AArch64 emulator and assembler, crucial for debugging and testing A64 assembly program that flashes the LED light on Raspberry Pi\n• Developed a piano simulator on Raspberry Pi, showcasing embedded systems programming skills',
+        link: 'https://github.com/shijua/c-project',
+      },
+      {
+        name: 'Bouncing Ball Game',
+        description:
+          '•	Created a bouncing ball game with 40 levels from easy to hard\n•	Included 8 different types of blocks that let ball reacts in different way',
+        link: 'https://github.com/shijua/bouncing-ball',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Dolby, London, UK',
+        description: 'Image Processing Intern, April 2024 - Sep 2024',
+        // link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'PATH Start-up, London, UK',
+        description: 'Software Engineering Internship, Jun 2024 - Sep 2024',
+        // link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'CassTime, Shenzhen, China',
+        description: 'Data Analyst Internship, August 2021',
+        // link: 'https://github.com/RyanFitzgerald/devfolio',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
@@ -60,16 +66,12 @@ module.exports = {
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Python, C, C++, Java, Dart, C#, PHP, Haskell, SQL, Rust, Kotlin, Prolog, Halide, Flutter, Flask',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
-      },
-      {
-        name: 'Other',
+        name: 'Languages',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'English (Fluent), Mandarin (Native), Japanese (Basic)',
       },
     ],
   },
